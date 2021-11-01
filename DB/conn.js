b157068de4c416a187eb5c6c749ac160c4fca023
Rohan <>
2021-11-01 11:40:13 +0530

    addded products

--- DB/conn.js
+++ DB/conn.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-// const DB='mongodb+srv://rohanrpawar:rohanrpawar@cluster0.t5bum.mongodb.net/ecommerce?retryWrites=true&w=majority';
 const DB = process.env.DATABASE
 
 mongoose.connect(DB)
